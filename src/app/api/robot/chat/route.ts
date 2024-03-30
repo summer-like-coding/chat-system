505b686475732c4dccb2eb32868606f7bc7da6b7
Sun-ZhenXing <1006925066@qq.com>
2024-03-30 18:25:29 +0800

    WIP: add openai api

---
+++ src/app/api/robot/chat/route.ts
@@ -0,0 +1,49 @@
+import type { NextRequest } from 'next/server'
+
+import { openai } from '@/lib/openai'
+
+export const runtime = 'nodejs'
+export const dynamic = 'force-dynamic'
+
+export async function GET(request: NextRequest) {
+  const responseStream = new TransformStream()
+  const writer = responseStream.writable.getWriter()
+  const encoder = new TextEncoder()
+  const url = new URL(request.nextUrl)
+  const prompt = url.searchParams.get('prompt') || ''
+
+  const openaiStream = await openai.chat.completions.create(
+    {
+      messages: [
+        {
+          content: prompt,
+          role: 'user',
+        },
+      ],
+      model: 'gpt-3.5-turbo',
+      stream: true,
+    },
+  );
+  (async () => {
+    try {
+      for await (const chunk of openaiStream) {
+        const chunkText = chunk.choices[0]?.delta?.content || ''
+        // process.stdout.write(chunkText)
+        await writer.write(encoder.encode(`data: ${chunkText}\n\n`))
+      }
+    }
+    catch (error) {
+      await writer.write(encoder.encode(`data: ERROR: ${error}\n\n`))
+    }
+    finally {
+      await writer.close()
+    }
+  })()
+  return new Response(responseStream.readable, {
+    headers: {
+      'Cache-Control': 'no-cache, no-transform',
+      'Connection': 'keep-alive',
+      'Content-Type': 'text/event-stream; charset=utf-8',
+    },
+  })
+}
