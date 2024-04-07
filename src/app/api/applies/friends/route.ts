ef5f1691003e4570c535c5a16436bc2c6dd5834c
Sun-ZhenXing <1006925066@qq.com>
2024-04-07 18:10:55 +0800

    WIP: add /api/applies/friends/

--- src/app/api/applies/friends/route.ts
+++ src/app/api/applies/friends/route.ts
@@ -1,3 +1,40 @@
+import type { NextRequest } from 'next/server'
+
+import { authOptions } from '@/lib/auth'
+import { friendApplyService } from '@/services/apply'
+import { Result } from '@/utils/result'
+import { getServerSession } from 'next-auth'
+import { getToken } from 'next-auth/jwt'
+
+/**
+ * @swagger
+ * definitions:
+ *  AppliesFriendsBody:
+ *   type: object
+ *   required:
+ *    - targetId
+ *    - userId
+ *   properties:
+ *     targetId:
+ *       type: string
+ *       description: 目标用户 ID
+ *     userId:
+ *       type: string
+ *       description: 用户 ID
+ *     type:
+ *       type: string
+ *       description: 申请类型
+ *     reason:
+ *       type: string
+ *       description: 申请理由
+ *     selfRemark:
+ *       type: string
+ *       description: 自己的备注
+ *     answer:
+ *       type: string
+ *       description: 回复
+ */
+
 /**
  * @swagger
  * /api/applies/friends/:
@@ -5,4 +42,59 @@
  *     summary: 申请加好友 @todo
  *     tags:
  *      - 申请
+ *     requestBody:
+ *       description: |
+ *        `{
+ *          targetId: string,
+ *          userId: string,
+ *          type?: string,
+ *          reason?: string,
+ *          selfRemark?: string,
+ *          answer?: string
+ *        }`
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             $ref: '#/definitions/AppliesFriendsBody'
+ *     responses:
+ *       200:
+ *         description: '`ResultType<ApplyVo>` 申请信息'
  */
+export async function POST(request: NextRequest) {
+  try {
+    const session = await getServerSession(authOptions)
+    if (!session) {
+      return Result.error('未登录')
+    }
+    const {
+      answer,
+      reason,
+      selfRemark,
+      targetId,
+      type,
+      userId,
+    } = await request.json()
+    if (!userId || !targetId) {
+      return Result.error('参数错误，缺少 userId 或 targetId')
+    }
+    const token = await getToken({ req: request })
+    if (token?.sub !== userId) {
+      return Result.error('无权限添加好友')
+    }
+    const apply = await friendApplyService.applyFor({
+      answer,
+      reason,
+      selfRemark,
+      targetId,
+      type,
+      userId,
+    })
+    return Result.success(apply)
+  }
+  catch (error: any) {
+    console.error('Error:', error)
+    return Result.error(`错误: ${error.message}`)
+  }
+}
--- src/services/apply.ts
+++ src/services/apply.ts
@@ -1,6 +1,5 @@
-import type { FriendApply, GroupApply } from '@prisma/client'
-
 import { prisma } from '@/lib/db'
+import { ApplyStatusType, type FriendApply, type GroupApply } from '@prisma/client'
 
 import { AbstractService } from './_base'
 
@@ -9,6 +8,59 @@ import { AbstractService } from './_base'
  */
 export class FriendApplyService extends AbstractService<FriendApply> {
   delegate = prisma.friendApply
+
+  /**
+   * 创建好友申请
+   */
+  async applyFor({ answer, reason, selfRemark, targetId, type, userId }: Pick<
+    Partial<FriendApply>,
+    'answer' | 'reason' | 'selfRemark' | 'targetId' | 'type' | 'userId'
+  >) {
+    return prisma.$transaction(async (ctx) => {
+      const user1 = await ctx.user.findUnique({
+        where: {
+          id: userId,
+          isDeleted: false,
+        },
+      })
+      if (!user1) {
+        throw new Error('用户不存在')
+      }
+      const user2 = await ctx.user.findUnique({
+        where: {
+          id: targetId,
+          isDeleted: false,
+        },
+      })
+      if (!user2) {
+        throw new Error('目标用户不存在')
+      }
+      if (user1.id === user2.id) {
+        throw new Error('不能添加自己为好友')
+      }
+      const apply = await ctx.friendApply.findFirst({
+        where: {
+          status: ApplyStatusType.PENDING,
+          targetId: user2.id,
+          userId: user1.id,
+        },
+      })
+      if (apply) {
+        throw new Error('已经申请过了，请等待申请结果')
+      }
+      const applyCreated = await ctx.friendApply.create({
+        data: {
+          answer,
+          reason,
+          selfRemark,
+          targetId: user2.id,
+          type,
+          userId: user1.id,
+        },
+      })
+      return applyCreated
+    })
+  }
 }
 
 export const friendApplyService = new FriendApplyService()
