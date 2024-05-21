33db77823ce40fb9ecf84bd4b98a7168b9b1cede
summer <3244742300@qq.com>
2024-05-21 20:34:39 +0800

    WIP: 加密

---
+++ src/app/utils/encry.ts
@@ -0,0 +1,68 @@
+/**
+ * 加密和解密
+ * @description 使用TweetNaCl.js库进行加密和解密
+ */
+import nacl from 'tweetnacl'
+import util from 'tweetnacl-util'
+/*
+** You'll need to generate a key pair for your users e.g.
+** const keypair = nacl.box.keyPair()
+** const receiverPublicKey = util.encodeBase64(keypair.publicKey)
+** const receiverSecretKey = util.encodeBase64(keypair.secretKey)
+**
+*/
+
+const keypair = nacl.box.keyPair()
+export const receiverPublicKey = util.encodeBase64(keypair.publicKey) // 公钥
+export const receiverSecretKey = util.encodeBase64(keypair.secretKey) // 私钥
+
+/* encrypted message interface */
+interface IEncryptedMsg {
+  ciphertext: string
+  ephemPubKey: string
+  nonce: string
+  version: string
+}
+/**
+ * 加密
+ * @param receiverPublicKey ；接收者的公钥
+ * @param msgParams：要加密的消息
+ * @returns
+ */
+export function encrypt(receiverPublicKey: string, msgParams: string) {
+  console.log('receiverPublicKey', receiverPublicKey)
+  console.log('msgParams', msgParams)
+
+  const ephemeralKeyPair = nacl.box.keyPair()
+  const pubKeyUInt8Array = util.decodeBase64(receiverPublicKey)
+  const msgParamsUInt8Array = util.decodeUTF8(msgParams)
+  const nonce = nacl.randomBytes(nacl.box.nonceLength)
+  const encryptedMessage = nacl.box(
+    msgParamsUInt8Array,
+    nonce,
+    pubKeyUInt8Array,
+    ephemeralKeyPair.secretKey,
+  )
+  return {
+    ciphertext: util.encodeBase64(encryptedMessage), // 加密后的消息
+    ephemPubKey: util.encodeBase64(ephemeralKeyPair.publicKey), // 临时公钥
+    nonce: util.encodeBase64(nonce), // nonce
+    version: 'x25519-xsalsa20-poly1305',
+  }
+}
+
+export function decrypt(receiverSecretKey: string, encryptedData: IEncryptedMsg) {
+  const receiverSecretKeyUint8Array = util.decodeBase64(
+    receiverSecretKey,
+  )
+  const nonce = util.decodeBase64(encryptedData.nonce)
+  const ciphertext = util.decodeBase64(encryptedData.ciphertext)
+  const ephemPubKey = util.decodeBase64(encryptedData.ephemPubKey)
+  const decryptedMessage = nacl.box.open(
+    ciphertext,
+    nonce,
+    ephemPubKey,
+    receiverSecretKeyUint8Array,
+  )
+  return util.encodeUTF8(decryptedMessage!)
+}
